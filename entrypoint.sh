305d9bcfd4681ef369f3bded6df347fa0c6d3f0a
rajuljha <rajuljha49@gmail.com>
2024-08-05 18:46:11 +0530

    test new

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,40 +7,39 @@ if [ -z "$INPUT_ENVIRONMENT" ] && [ -z "$INPUT_REQUIREMENTS" ] && [ -z "$INPUT_P
 fi
 
 # Set the working directory to /github/workspace
-WORKDIR=$GITHUB_WORKSPACE
+WORKDIR=/github/workspace
 SBOM_DIR="$WORKDIR/sbom"
 mkdir -p "$SBOM_DIR"
 
 # Determine which command to run based on inputs
-command="cyclonedx-py"
 sbom_file=""
 
 if [ "$INPUT_ENVIRONMENT" = "true" ]; then
   echo "Generating SBOM for environment..."
   sbom_file="${SBOM_DIR}/sbom_env.json"
-  python -m cyclonedx_py environment > "$sbom_file"
+  python -m cyclonedx-py environment $GITHUB_WORKSPACE > "$sbom_file"
 elif [ "$INPUT_REQUIREMENTS" = "true" ]; then
   echo "Generating SBOM for requirements..."
   sbom_file="${SBOM_DIR}/sbom_req.json"
-  python -m cyclonedx_py requirements > "$sbom_file"
+  python -m cyclonedx-py requirements "$GITHUB_WORKSPACE/requirements.txt" > "$sbom_file"
 elif [ "$INPUT_PIPENV" = "true" ]; then
   echo "Generating SBOM for pipenv..."
   sbom_file="${SBOM_DIR}/sbom_pipenv.json"
-  python -m cyclonedx_py pipenv > "$sbom_file"
+  python -m cyclonedx-py pipenv "$GITHUB_WORKSPACE" > "$sbom_file"
 elif [ "$INPUT_POETRY" = "true" ]; then
   echo "Generating SBOM for poetry..."
   sbom_file="${SBOM_DIR}/sbom_poetry.json"
-  python -m cyclonedx_py poetry > "$sbom_file"
+  python -m cyclonedx-py poetry "$GITHUB_WORKSPACE" > "$sbom_file"
 fi
 
 # List files for debugging
 echo "Files in SBOM directory:"
 ls -l "$SBOM_DIR"
 
-# Set the GitHub action output variable for the path of the SBOM file
-if [ -n "$sbom_file" ]; then
-#   echo "::set-output name=sbom_file_path::$sbom_file"
-  echo "sbom_file_path=$sbom_file" >> $GITHUB_OUTPUT
-else
-  echo "No SBOM file was generated."
-fi
+# # Set the GitHub action output variable for the path of the SBOM file
+# if [ -n "$sbom_file" ]; then
+# #   echo "::set-output name=sbom_file_path::$sbom_file"
+#   echo "sbom_file_path=$sbom_file" >> $GITHUB_OUTPUT
+# else
+#   echo "No SBOM file was generated."
+# fi
