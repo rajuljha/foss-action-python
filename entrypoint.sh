d4620e67ab3a92fc56f7b97ce56e789419b2179d
rajuljha <rajuljha49@gmail.com>
2024-08-05 19:07:01 +0530

    woah

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,33 +8,30 @@ fi
 
 # Set the working directory to /github/workspace
 WORKDIR=/github/workspace
-SBOM_DIR="$WORKDIR/sbom"
-mkdir -p "$SBOM_DIR"
+# SBOM_DIR="$WORKDIR/sbom"
+# mkdir -p "$SBOM_DIR"
 
 # Determine which command to run based on inputs
-sbom_file=""
+sbom_file="$WORKDIR/sbom_py.json"
 
 if [ "$INPUT_ENVIRONMENT" = "true" ]; then
   echo "Generating SBOM for environment..."
-  sbom_file="${SBOM_DIR}/sbom_py.json"
   python -m cyclonedx-py environment $GITHUB_WORKSPACE > "$sbom_file"
 elif [ "$INPUT_REQUIREMENTS" = "true" ]; then
   echo "Generating SBOM for requirements..."
-  sbom_file="${SBOM_DIR}/sbom_py.json"
-  cyclonedx-py requirements "$GITHUB_WORKSPACE/requirements.txt" -o "$sbom_file"
+  cyclonedx-py requirements "$GITHUB_WORKSPACE/requirements.txt" > "$sbom_file"
 elif [ "$INPUT_PIPENV" = "true" ]; then
   echo "Generating SBOM for pipenv..."
-  sbom_file="${SBOM_DIR}/sbom_py.json"
   python -m cyclonedx-py pipenv "$GITHUB_WORKSPACE" > "$sbom_file"
 elif [ "$INPUT_POETRY" = "true" ]; then
   echo "Generating SBOM for poetry..."
-  sbom_file="${SBOM_DIR}/sbom_py.json"
   python -m cyclonedx-py poetry "$GITHUB_WORKSPACE" > "$sbom_file"
 fi
 
 # List files for debugging
 echo "Files in SBOM directory:"
-ls -l "$SBOM_DIR"
+ls -l "$WORKDIR"
+ls -l "$GITHUB_WORKSPACE"
 
 # # Set the GitHub action output variable for the path of the SBOM file
 # if [ -n "$sbom_file" ]; then
